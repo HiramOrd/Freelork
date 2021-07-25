f09462fad2ca9d8b2062babf6dc884cfcd2a656c
Erick Espinoza <erickesp2077@gmail.com>
2021-07-24 19:50:24 -0500

    Fix(teacher-tables)

--- src/app/pages/teacher/home-teacher/home-teacher.component.ts
+++ src/app/pages/teacher/home-teacher/home-teacher.component.ts
@@ -3,7 +3,12 @@ import {TableService} from '../../../utilities/tables/table.service';
 import {Observable} from 'rxjs';
 import {NgbdSortableHeader, SortEvent} from '../../../utilities/tables/sortable.directive';
 import {StudentsService} from '../../students/students.service';
-import {Tasks} from '../../../variables/tasks';
+import {studentTasks} from '../../../variables/studentTasks';
+import {UtilitiesService} from '../../../utilities/utilities.service';
+import {ExportExcelService} from '../../../utilities/export-excel.service';
+import {ToastService} from '../../../utilities/toast.service';
+
+
 
 @Component({
   selector: 'app-home-teacher',
@@ -11,31 +16,37 @@ import {Tasks} from '../../../variables/tasks';
   styleUrls: ['./home-teacher.component.css']
 })
 export class HomeTeacherComponent implements OnInit {
-  today;
-  dateMinRange;
-  dateMaxRange;
-  dateDisabled = true;
+  today = new Date();
+  dateMinRange = null;
+  dateMaxRange = null;
   public isCollapsed = true;
 
   // Table
+  table;
   arrayTable$: Observable<any[]>;
   total$: Observable<number>;
   @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
 
-  constructor(public studentsService: StudentsService, public tableService: TableService) {
-    this.today = Date.now();
-    this.dateMinRange = this.today;
-    this.dateMaxRange = this.today;
+  constructor(
+    public studentsService: StudentsService,
+    private toastService: ToastService,
+    // Table
+    public tableService: TableService,
+    public utilitiesService: UtilitiesService,
+    public exportExcelService: ExportExcelService,
+  ) {
   }
 
   ngOnInit(): void {
-    this.setTableInfo(Tasks);
+    this.setTableInfo(studentTasks);
+    this.arrayTable$ = this.tableService.arrayTable$;
+    this.total$ = this.tableService.total$;
   }
 
   setTableInfo(arrayTable) {
-    this.tableService.initService(arrayTable);
-    this.arrayTable$ = this.tableService.arrayTable$;
-    this.total$ = this.tableService.total$;
+    this.table = arrayTable;
+    this.table = this.utilitiesService.statusTaskToString(this.table, 'status');
+    this.tableService.initService(this.table);
   }
 
   onSort({column, direction}: SortEvent) {
@@ -50,4 +61,49 @@ export class HomeTeacherComponent implements OnInit {
     this.tableService.sortDirection = direction;
   }
 
+  resetFilters () {
+    this.dateMinRange = null;
+    this.dateMaxRange = null;
+    this.tableService.searchTerm = '';
+    this.setTableInfo(studentTasks);
+  }
+
+  changeFilter () {
+    if (this.dateMaxRange === null) {
+    } else if (this.dateMaxRange && this.dateMinRange) {
+      this.setTableInfo(studentTasks);
+    }
+  }
+
+  exportAll() {
+    // Endpoint To export
+    this.exportToExcel('Todo');
+  }
+  exportByDate() {
+    if (this.dateMaxRange && this.dateMinRange) {
+      // Endpoint To export
+      this.exportToExcel('Por fecha');
+    } else {
+      this.toastService.show('No tienes un rango de fechas seleccionado' , { classname: 'bg-danger text-white'});
+    }
+}
+
+  exportToExcel(type: string) {
+    let arrayToExport = this.table;
+    arrayToExport = this.utilitiesService.deleteColumn('id', arrayToExport);
+    arrayToExport = this.utilitiesService.deleteColumn('idProject', arrayToExport);
+    const dataForExcel = [];
+    arrayToExport.forEach((row: any) => {
+      dataForExcel.push(Object.values(row));
+    });
+
+    const reportData = {
+      title: 'Estancias_' + type,
+      data: dataForExcel,
+      headers: ['Titulo', 'Alumno', 'Empresa', 'Proyecto', 'Fecha', 'Horas', 'Estado'],
+      sizeColumns: [50, 25, 15, 20, 10, 5, 15]
+    };
+
+    this.exportExcelService.exportExcel(reportData);
+  }
 }
--- src/app/pages/teacher/students/students-list/students-list.component.ts
+++ src/app/pages/teacher/students/students-list/students-list.component.ts
@@ -2,8 +2,11 @@ import {Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {TableService} from '../../../../utilities/tables/table.service';
 import {Observable} from 'rxjs';
 import {NgbdSortableHeader, SortEvent} from '../../../../utilities/tables/sortable.directive';
-import {Tasks} from '../../../../variables/tasks';
+import {studentList} from '../../../../variables/studentList';
 import {StudentsService} from '../../../students/students.service';
+import {UtilitiesService} from '../../../../utilities/utilities.service';
+import {ExportExcelService} from '../../../../utilities/export-excel.service';
+import {ToastService} from '../../../../utilities/toast.service';
 
 @Component({
   selector: 'app-students-list',
@@ -11,31 +14,37 @@ import {StudentsService} from '../../../students/students.service';
   styleUrls: ['./students-list.component.css']
 })
 export class StudentsListComponent implements OnInit {
-  today;
-  dateMinRange;
-  dateMaxRange;
-  dateDisabled = true;
+  today = new Date();
+  dateMinRange = null;
+  dateMaxRange = null;
   public isCollapsed = true;
 
   // Table
+  table;
   arrayTable$: Observable<any[]>;
   total$: Observable<number>;
   @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
 
-  constructor(public studentsService: StudentsService, public tableService: TableService) {
-    this.today = Date.now();
-    this.dateMinRange = this.today;
-    this.dateMaxRange = this.today;
+  constructor(
+    public studentsService: StudentsService,
+    private toastService: ToastService,
+    // Table
+    public tableService: TableService,
+    public utilitiesService: UtilitiesService,
+    public exportExcelService: ExportExcelService,
+  ) {
   }
 
   ngOnInit(): void {
-    this.setTableInfo(Tasks);
+    this.setTableInfo(studentList);
+    this.arrayTable$ = this.tableService.arrayTable$;
+    this.total$ = this.tableService.total$;
   }
 
   setTableInfo(arrayTable) {
-    this.tableService.initService(arrayTable);
-    this.arrayTable$ = this.tableService.arrayTable$;
-    this.total$ = this.tableService.total$;
+    this.table = arrayTable;
+   /*  this.table = this.utilitiesService.statusTaskToString(this.table, 'status'); */
+    this.tableService.initService(this.table);
   }
 
   onSort({column, direction}: SortEvent) {
@@ -49,5 +58,49 @@ export class StudentsListComponent implements OnInit {
     this.tableService.sortColumn = column;
     this.tableService.sortDirection = direction;
   }
+/* 
+  resetFilters () {
+    this.dateMinRange = null;
+    this.dateMaxRange = null;
+    this.tableService.searchTerm = '';
+    this.setTableInfo(studentList);
+  }
 
+  changeFilter () {
+    if (this.dateMaxRange === null) {
+    } else if (this.dateMaxRange && this.dateMinRange) {
+      this.setTableInfo(studentList);
+    }
+  } */
+
+  exportAll() {
+    // Endpoint To export
+    this.exportToExcel('Todo');
+  }
+/*   exportByDate() {
+    if (this.dateMaxRange && this.dateMinRange) {
+      // Endpoint To export
+      this.exportToExcel('Por fecha');
+    } else {
+      this.toastService.show('No tienes un rango de fechas seleccionado' , { classname: 'bg-danger text-white'});
+    }
+} */
+
+  exportToExcel(type: string) {
+    let arrayToExport = this.table;
+    arrayToExport = this.utilitiesService.deleteColumn('idStudent', arrayToExport);
+    const dataForExcel = [];
+    arrayToExport.forEach((row: any) => {
+      dataForExcel.push(Object.values(row));
+    });
+
+    const reportData = {
+      title: 'Lista_Alumnos_' + type,
+      data: dataForExcel,
+      headers: ['Matricula', 'Alumno', 'Grupo', 'Empresa', 'Asesor', 'Horas trabajadas'],
+      sizeColumns: [15, 25, 15, 20, 10, 25]
+    };
+
+    this.exportExcelService.exportExcel(reportData);
+  }
 }
